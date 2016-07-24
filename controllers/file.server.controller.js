c8a38921caf7693f92df7dd2dbf8f5b5beb166ba
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-24 18:26:03 +0200

    add renderFileForm to file controller

--- controllers/file.server.controller.js
+++ controllers/file.server.controller.js
@@ -10,3 +10,7 @@ exports.processFile = upload.single('userFile');
 exports.sendFileSize = (req, res, next) => {
   res.send(req.file);
 }
+
+exports.renderFileForm = (req, res) => {
+  res.render('../views/pages/index');
+}
