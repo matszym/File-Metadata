3802ade895f8c94df964c6f763b8a23587b8806b
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-24 18:19:11 +0200

    Add express routes for file upload controller

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 let express = require('express'),
 app = express(),
 config = require('./config/config.js'),
-server;
+server,
+fileCtrl = require('./controllers/file.server.controller.js');
+
+app.use('/upload', fileCtrl.processFile, fileCtrl.sendFileSize);
 
 server = app.listen(config.port);
 console.log('Server is listening on', config.port);
