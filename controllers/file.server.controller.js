851f6ceb53b4fb3b53206bfeb4ba12b7250e3d8d
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-24 22:44:31 +0200

    File processing implementation

--- controllers/file.server.controller.js
+++ controllers/file.server.controller.js
@@ -1,14 +1,32 @@
 'use strict';
 
 let multer = require('multer'),
+fs = require('fs'),
 upload = multer({
   dest: 'uploads/'
-});
+}),
+Promise = require('bluebird');
 
-exports.processFile = upload.single('userFile');
+exports.processFile = (req, res, next) => {
+  let fileUpload = Promise.promisify(upload.single('userfile')),
+  removeFile = Promise.promisify(fs.unlink);
+
+  fileUpload(req, res)
+  .then(() => {
+    return removeFile('./uploads/' + req.file.filename);
+  })
+  .then(() => {
+    next();
+  })
+  .catch(err => {
+    res.send(err);
+  });
+}
 
 exports.sendFileSize = (req, res, next) => {
-  res.send(req.file);
+  res.send({
+    size: req.file.size
+  });
 }
 
 exports.renderFileForm = (req, res) => {
